471d21fe39f88bdb671a0a639a652e5ddb2ffaec
leek <leeked@gmail.com>
2012-04-01 00:34:45 -0400

    Move test constants into phpunit.xml.dist

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -6,5 +6,6 @@ if (file_exists($file = __DIR__.'/../autoload.php')) {
     require_once $file;
 }
 
-define('TESTS_BASEPATH', realpath(dirname(__FILE__) . '/Bronto/Tests'));
-define('TEST_API_TOKEN', '4EEE0D9E-101E-4E15-99D5-DF4648199C18');
+if (!((bool) TEST_API_TOKEN)) {
+    throw new RuntimeException('You must specify an API token to test with in phpunit.xml');
+}
