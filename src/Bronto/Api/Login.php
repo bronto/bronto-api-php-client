e9f6e29324b5ca6863bc9a6c299cee39690fffe0
leek <leeked@gmail.com>
2012-04-01 04:45:31 -0400

    Adding loginObject

---
+++ src/Bronto/Api/Login.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * @author Chris Jones <chris.jones@bronto.com>
+ * @link http://community.bronto.com/api/v4/objects/general/accountobject
+ *
+ * @method Bronto_Api_Login_Row createRow() createRow(array $data)
+ */
+class Bronto_Api_Login extends Bronto_Api_Object
+{
+    /**
+     * @var array
+     */
+    protected $_methods = array(
+        'addLogins'    => 'add',
+        'readLogins'   => 'read',
+        'updateLogins' => 'update',
+        'deleteLogins' => 'delete',
+    );
+
+    /**
+     * @param array $filter
+     * @param int $pageNumber
+     * @return Bronto_Api_Rowset
+     */
+    public function readAll(array $filter = array(), $pageNumber = 1)
+    {
+        $params = array();
+        $params['filter']     = $filter;
+        $params['pageNumber'] = (int) $pageNumber;
+        return $this->read($params);
+    }
+}
---
+++ src/Bronto/Api/Login/Row.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * @property string $username
+ * @property-write string $password
+ * @property array $contactInformation
+ * @property bool $permissionAgencyAdmin
+ * @property bool $permissionAdmin
+ * @property bool $permissionApi
+ * @property bool $permissionUpgrade
+ * @property bool $permissionFatigueOverride
+ * @property bool $permissionMessageCompose
+ * @property bool $permissionMessageDelete
+ * @property bool $permissionAutomatorCompose
+ * @property bool $permissionListCreateSend
+ * @property bool $permissionListCreate
+ * @property bool $permissionSegmentCreate
+ * @property bool $permissionFieldCreate
+ * @property bool $permissionFieldReorder
+ * @property bool $permissionSubscriberCreate
+ * @property bool $permissionSubscriberView
+ * @method Bronto_Api_Login_Row read() read()
+ * @method Bronto_Api_Login_Row save() save()
+ * @method Bronto_Api_Login_Row delete() delete()
+ * @method Bronto_Api_Login getApiObject() getApiObject()
+ */
+class Bronto_Api_Login_Row extends Bronto_Api_Row
+{
+
+}
