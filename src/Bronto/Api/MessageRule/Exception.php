f39fb2e4f4b7ae47570c2aa100ef7869c08d998a
leek <leeked@gmail.com>
2012-04-10 10:48:43 -0400

    Adding renamed

---
+++ src/Bronto/Api/MessageRule/Exception.php
@@ -0,0 +1,10 @@
+<?php
+
+class Bronto_Api_MessageRule_Exception extends Bronto_Api_Exception
+{
+    const INVALID_AUTOMATOR        = 604; // The specified automator is invalid.
+    const INVALID_AUTOMATOR_NAME   = 610; // The message rule name is invalid.
+    const INVALID_AUTOMATOR_TYPE   = 611; // The message rule type is invalid.
+    const INVALID_AUTOMATOR_STATUS = 612; // The message rule status is invalid.
+    const AUTOMATOR_EXISTS         = 613; // A message rule with this name already exists: %s
+}
---
+++ src/Bronto/Api/MessageRule/Row.php
@@ -0,0 +1,56 @@
+<?php
+
+/**
+ * @property-read id
+ * @property name
+ * @property type
+ * @property messagedId
+ * @method Bronto_Api_MessageRule_Row delete() delete()
+ * @method Bronto_Api_MessageRule getApiObject() getApiObject()
+ */
+class Bronto_Api_MessageRule_Row extends Bronto_Api_Row
+{
+    /**
+     * @param bool $returnData
+     * @return Bronto_Api_MessageRule_Row
+     */
+    public function read()
+    {
+        if ($this->id) {
+            $params = array('id' => $this->id);
+        } else {
+            $params = array(
+                'name' => array(
+                    'value'    => $this->name,
+                    'operator' => 'EqualTo',
+                )
+            );
+        }
+
+        return parent::_read($params, $returnData);
+    }
+
+    /**
+     * @param bool $upsert
+     * @param bool $refresh
+     * @return Bronto_Api_MessageRule_Row
+     */
+    public function save($upsert = true, $refresh = false)
+    {
+        if (!$upsert) {
+            parent::_save(false, $refresh);
+        }
+
+        try {
+            parent::_save(true, $refresh);
+        } catch (Bronto_Api_MessageRule_Exception $e) {
+            if ($e->getCode() === Bronto_Api_MessageRule_Exception::AUTOMATOR_EXISTS) {
+                $this->_refresh();
+            } else {
+                $this->getApi()->throwException($e);
+            }
+        }
+
+        return $this;
+    }
+}
