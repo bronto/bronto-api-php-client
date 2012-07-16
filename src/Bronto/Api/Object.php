3e8e1f25b1e47784993e46c7611a3eeb8a6e9906
Chris Jones <leeked@gmail.com>
2012-07-16 14:22:59 -0400

    Removing debug...

--- src/Bronto/Api/Object.php
+++ src/Bronto/Api/Object.php
@@ -329,8 +329,8 @@ abstract class Bronto_Api_Object
 
             try {
                 // Store this request in case we need to retry later
-                $this->_lastRequestMethod = $method; d($method);
-                $this->_lastRequestData   = $data; d($data);
+                $this->_lastRequestMethod = $method;
+                $this->_lastRequestData   = $data;
 
                 // Attempt
                 $client = $this->getApi()->getSoapClient();
--- tests/Bronto/Tests/Api/Order/OrderTest.php
+++ tests/Bronto/Tests/Api/Order/OrderTest.php
@@ -6,34 +6,110 @@
  */
 class Bronto_Tests_Api_Order_OrderTest extends Bronto_Tests_AbstractTest
 {
-    protected $apiToken  = TEST_API_TOKEN_2;
+    protected $apiToken = TEST_API_TOKEN_1;
 
-    public function testAddUpdateOrder()
+    public function testAddUpdateOrderUsingExitingContactEmail()
     {
         $orderObject = $this->getObject();
 
         /* @var $brontoOrder Bronto_Api_Order_Row */
         $brontoOrder = $orderObject->createRow();
         $brontoOrder->email       = 'leeked@gmail.com';
-        $brontoOrder->orderId     = 1;
+        $brontoOrder->id          = 1;
         $brontoOrder->createdDate = date('c', time());
         $brontoOrder->items       = array(
             array(
-               'sku'         => 'sku1',
-               'description' => 'desc1',
-               'quantity'    => 1,
-               'amount'      => 1.00,
+              'id'          => '1001',
+              'sku'         => 'sku1',
+              'description' => 'desc1',
+              'quantity'    => 1,
+              'amount'      => 1.00,
             ),
             array(
-               'sku'         => 'sku2',
-               'description' => 'desc2',
-               'quantity'    => 1,
-               'amount'      => 2.00,
+              'id'          => '1002',
+              'sku'         => 'sku2',
+              'description' => 'desc2',
+              'quantity'    => 1,
+              'amount'      => 2.00,
             )
         );
-        $result = $brontoOrder->save();
 
-        var_dump($result); exit;
+        try {
+          $result = $brontoOrder->save();
+          d($result);
+        } catch (Exception $e) {
+          echo $e->getMessage() . "\n";
+          echo $orderObject->getApi()->getLastRequest() . "\n";
+        }
+    }
+
+    public function testAddUpdateOrderUsingNewContactEmail()
+    {
+        $orderObject = $this->getObject();
+
+        /* @var $brontoOrder Bronto_Api_Order_Row */
+        $brontoOrder = $orderObject->createRow();
+        $brontoOrder->email       = 'leeked+addupdateorder' . rand() . '@gmail.com';
+        $brontoOrder->id          = 2;
+        $brontoOrder->createdDate = date('c', time());
+        $brontoOrder->items       = array(
+            array(
+              'id'          => '1001',
+              'sku'         => 'sku1',
+              'description' => 'desc1',
+              'quantity'    => 1,
+              'amount'      => 1.00,
+            ),
+            array(
+              'id'          => '1002',
+              'sku'         => 'sku2',
+              'description' => 'desc2',
+              'quantity'    => 1,
+              'amount'      => 2.00,
+            )
+        );
+
+        try {
+          $result = $brontoOrder->save();
+          d($result);
+        } catch (Exception $e) {
+          echo $e->getMessage() . "\n";
+          echo $orderObject->getApi()->getLastRequest() . "\n";
+        }
+    }
+
+    public function testAddUpdateOrderUsingContactId()
+    {
+        $orderObject = $this->getObject();
+
+        /* @var $brontoOrder Bronto_Api_Order_Row */
+        $brontoOrder = $orderObject->createRow();
+        $brontoOrder->contactId   = '551dea25e-0b54-402a-af4c-c8947cf549a8';
+        $brontoOrder->id          = 2;
+        $brontoOrder->createdDate = date('c', time());
+        $brontoOrder->items       = array(
+            array(
+              'id'          => '1001',
+              'sku'         => 'sku1',
+              'description' => 'desc1',
+              'quantity'    => 1,
+              'amount'      => 1.00,
+            ),
+            array(
+              'id'          => '1002',
+              'sku'         => 'sku2',
+              'description' => 'desc2',
+              'quantity'    => 1,
+              'amount'      => 2.00,
+            )
+        );
+
+        try {
+          $result = $brontoOrder->save();
+        } catch (Exception $e) {
+          echo $e->getMessage() . "\n";
+          echo $orderObject->getApi()->getLastRequest() . "\n";
+        }
     }
 
     /**
