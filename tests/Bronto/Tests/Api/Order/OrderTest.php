269ae22a407c49f313d83075bee28d48dfe22679
Chris Jones <leeked@gmail.com>
2012-10-10 03:10:54 -0400

    Adding a persist/flush test for Orders

--- tests/Bronto/Tests/Api/Order/OrderTest.php
+++ tests/Bronto/Tests/Api/Order/OrderTest.php
@@ -8,16 +8,80 @@ class Bronto_Tests_Api_Order_OrderTest extends Bronto_Tests_AbstractTest
 {
     protected $apiToken = TEST_API_TOKEN_1;
 
+    /**
+     * @covers Bronto_Api_Order::flush()
+     */
+    public function testPersistCapturesErrors()
+    {
+        $orderObject = $this->getObject();
+
+        /* @var $order1 Bronto_Api_Order_Row */
+        $order1 = $orderObject->createRow();
+        $order1->email       = 'leeked+testpersist' . time() . '@gmail.com';
+        $order1->id          = rand();
+        $order1->orderDate   = date('c', time());
+        $order1->products    = array(
+            array(
+              'id'          => '1001',
+              'sku'         => 'sku1',
+              'description' => 'desc1',
+              'quantity'    => 1,
+              'price'       => 1.00,
+            ),
+            array(
+              'id'          => '1002',
+              'sku'         => 'sku2',
+              'description' => 'desc2',
+              'quantity'    => 1,
+              'price'       => 2.00,
+            )
+        );
+        $order1->persist();
+
+        /* @var $order2 Bronto_Api_Order_Row */
+        $order2 = $orderObject->createRow();
+        $order2->email       = 'leeked+testpersist' . time() . '@gmail.com';
+        $order2->id          = rand();
+        $order2->tid         = rand();
+        $order2->orderDate   = date('c', time());
+        $order2->products    = array(
+            array(
+              'id'          => '1001',
+              'sku'         => 'sku1',
+              'description' => 'desc1',
+              'quantity'    => 1,
+              'price'       => 1.00,
+            ),
+            array(
+              'id'          => '1002',
+              'sku'         => 'sku2',
+              'description' => 'desc2',
+              'quantity'    => 1,
+              'price'       => 2.00,
+            )
+        );
+        $order2->persist();
+
+        $result = $orderObject->flush();
+
+        $this->assertTrue($result->hasErrors());
+        $this->assertTrue(is_array($result->getErrors()));
+        $this->assertTrue(count($result->getErrors()) === 1);
+    }
+
+    /**
+     * @covers Bronto_Api_Order_Row::save()
+     */
     public function testAddUpdateOrderUsingExitingContactEmail()
     {
         $orderObject = $this->getObject();
 
-        /* @var $brontoOrder Bronto_Api_Order_Row */
-        $brontoOrder = $orderObject->createRow();
-        $brontoOrder->email       = 'leeked@gmail.com';
-        $brontoOrder->id          = rand();
-        $brontoOrder->createdDate = date('c', time());
-        $brontoOrder->items       = array(
+        /* @var $order Bronto_Api_Order_Row */
+        $order = $orderObject->createRow();
+        $order->email       = 'leeked@gmail.com';
+        $order->id          = rand();
+        $order->orderDate   = date('c', time());
+        $order->items       = array(
             array(
               'id'          => '1001',
               'sku'         => 'sku1',
@@ -33,26 +97,26 @@ class Bronto_Tests_Api_Order_OrderTest extends Bronto_Tests_AbstractTest
               'amount'      => 2.00,
             )
         );
+        $result = $order->save();
 
-        try {
-          $result = $brontoOrder->save();
-          d($result);
-        } catch (Exception $e) {
-          echo $e->getMessage() . "\n";
-          echo $orderObject->getApi()->getLastRequest() . "\n";
-        }
+        $this->assertTrue($order instanceOf Bronto_Api_Order_Row);
+        $this->assertSame(spl_object_hash($order), spl_object_hash($result));
+        $this->assertNotEmpty($order->id);
     }
 
+    /**
+     * @covers Bronto_Api_Order_Row::save()
+     */
     public function testAddUpdateOrderUsingNewContactEmail()
     {
         $orderObject = $this->getObject();
 
-        /* @var $brontoOrder Bronto_Api_Order_Row */
-        $brontoOrder = $orderObject->createRow();
-        $brontoOrder->email       = 'leeked+addupdateorder' . rand() . '@gmail.com';
-        $brontoOrder->id          = rand();
-        $brontoOrder->createdDate = date('c', time());
-        $brontoOrder->items       = array(
+        /* @var $order Bronto_Api_Order_Row */
+        $order = $orderObject->createRow();
+        $order->email       = 'leeked+addupdateorder' . rand() . '@gmail.com';
+        $order->id          = rand();
+        $order->orderDate   = date('c', time());
+        $order->items       = array(
             array(
               'id'          => '1001',
               'sku'         => 'sku1',
@@ -68,26 +132,26 @@ class Bronto_Tests_Api_Order_OrderTest extends Bronto_Tests_AbstractTest
               'amount'      => 2.00,
             )
         );
+        $result = $order->save();
 
-        try {
-          $result = $brontoOrder->save();
-          d($result);
-        } catch (Exception $e) {
-          echo $e->getMessage() . "\n";
-          echo $orderObject->getApi()->getLastRequest() . "\n";
-        }
+        $this->assertTrue($order instanceOf Bronto_Api_Order_Row);
+        $this->assertSame(spl_object_hash($order), spl_object_hash($result));
+        $this->assertNotEmpty($order->id);
     }
 
+    /**
+     * @covers Bronto_Api_Order_Row::save()
+     */
     public function testAddUpdateOrderUsingContactId()
     {
         $orderObject = $this->getObject();
 
-        /* @var $brontoOrder Bronto_Api_Order_Row */
-        $brontoOrder = $orderObject->createRow();
-        $brontoOrder->contactId   = '551dea25e-0b54-402a-af4c-c8947cf549a8';
-        $brontoOrder->id          = rand();
-        $brontoOrder->createdDate = date('c', time());
-        $brontoOrder->items       = array(
+        /* @var $order Bronto_Api_Order_Row */
+        $order = $orderObject->createRow();
+        $order->contactId   = '551dea25e-0b54-402a-af4c-c8947cf549a8';
+        $order->id          = rand();
+        $order->orderDate   = date('c', time());
+        $order->items       = array(
             array(
               'id'          => '1001',
               'sku'         => 'sku1',
@@ -104,12 +168,11 @@ class Bronto_Tests_Api_Order_OrderTest extends Bronto_Tests_AbstractTest
             )
         );
 
-        try {
-          $result = $brontoOrder->save();
-        } catch (Exception $e) {
-          echo $e->getMessage() . "\n";
-          echo $orderObject->getApi()->getLastRequest() . "\n";
-        }
+        $result = $order->save();
+
+        $this->assertTrue($order instanceOf Bronto_Api_Order_Row);
+        $this->assertSame(spl_object_hash($order), spl_object_hash($result));
+        $this->assertNotEmpty($order->id);
     }
 
     /**
