921d486e49e87ed9f3be80511ce1c2e9b13620fa
brontodavejohnson <dave.johnson@bronto.com>
2013-04-08 18:16:56 -0300

    Added copyright and license

--- src/Bronto/SoapClient.php
+++ src/Bronto/SoapClient.php
@@ -2,6 +2,8 @@
 
 /**
  * @author Jared Hodak <jhodak@kadro.com>
+ * @copyright  2011-2013 Bronto Software, Inc.
+ * @license http://opensource.org/licenses/OSL-3.0 Open Software License v. 3.0 (OSL-3.0)
  */
 class Bronto_SoapClient extends SoapClient
 {
