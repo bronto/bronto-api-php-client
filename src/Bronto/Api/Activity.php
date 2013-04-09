a51ceda5aac7c8cad6b95ea5c04743c156f5aff3
brontodavejohnson <dave.johnson@bronto.com>
2013-04-09 16:12:35 -0300

    Added copyright and license

--- src/Bronto/Api/Activity.php
+++ src/Bronto/Api/Activity.php
@@ -2,6 +2,8 @@
 
 /**
  * @author Chris Jones <chris.jones@bronto.com>
+ * @copyright  2011-2013 Bronto Software, Inc.
+ * @license http://opensource.org/licenses/OSL-3.0 Open Software License v. 3.0 (OSL-3.0)
  * @link http://community.bronto.com/api/v4/objects/general/activityobject
  */
 class Bronto_Api_Activity extends Bronto_Api_Object
