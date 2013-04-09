33486d613a51838cb1bb7bea1ff4792488c6c81b
brontodavejohnson <dave.johnson@bronto.com>
2013-04-09 16:25:34 -0300

    Added copyright and license

--- src/Bronto/Api/Field/TypeGuesser.php
+++ src/Bronto/Api/Field/TypeGuesser.php
@@ -2,6 +2,9 @@
 
 /**
  * @author Chris Jones <chris.jones@bronto.com>
+ * @copyright  2011-2013 Bronto Software, Inc.
+ * @license http://opensource.org/licenses/OSL-3.0 Open Software License v. 3.0 (OSL-3.0)
+ * 
  */
 class Bronto_Api_Field_TypeGuesser
 {
