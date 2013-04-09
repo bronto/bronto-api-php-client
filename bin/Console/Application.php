5bd0e1b45d8d4889b79ee223b1836588fcdbc2ac
brontodavejohnson <dave.johnson@bronto.com>
2013-04-09 17:30:13 -0300

    Update Application.php

--- bin/Console/Application.php
+++ bin/Console/Application.php
@@ -1,5 +1,8 @@
 <?php
-
+/**
+ * @copyright  2011-2013 Bronto Software, Inc.
+ * @license http://opensource.org/licenses/OSL-3.0 Open Software License v. 3.0 (OSL-3.0)
+ */
 namespace Console;
 
 class Application extends \Symfony\Component\Console\Application
