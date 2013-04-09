1f6e4fc3fdc554bdfc6bed71a490c18a8f1da305
brontodavejohnson <dave.johnson@bronto.com>
2013-04-09 16:21:59 -0300

    Added copyright and license

--- src/Bronto/Api/MessageRule/Exception.php
+++ src/Bronto/Api/MessageRule/Exception.php
@@ -1,5 +1,9 @@
 <?php
-
+/**
+ * 
+ * @copyright  2011-2013 Bronto Software, Inc.
+ * @license http://opensource.org/licenses/OSL-3.0 Open Software License v. 3.0 (OSL-3.0)
+ */
 class Bronto_Api_MessageRule_Exception extends Bronto_Api_Exception
 {
     const INVALID_AUTOMATOR        = 604; // The specified automator is invalid.
