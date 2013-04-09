86d088fee4374f6409dc68851c8ae57a708fede1
brontodavejohnson <dave.johnson@bronto.com>
2013-04-09 16:21:09 -0300

    Added copyright and license

--- src/Bronto/Api/Order/Exception.php
+++ src/Bronto/Api/Order/Exception.php
@@ -1,5 +1,9 @@
 <?php
-
+/**
+ * 
+ * @copyright  2011-2013 Bronto Software, Inc.
+ * @license http://opensource.org/licenses/OSL-3.0 Open Software License v. 3.0 (OSL-3.0)
+ */
 class Bronto_Api_Order_Exception extends Bronto_Api_Exception
 {
 
