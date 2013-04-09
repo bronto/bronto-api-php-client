55f334779d41edfda6a34e9bb93c83f91dc72cac
brontodavejohnson <dave.johnson@bronto.com>
2013-04-09 17:36:02 -0300

    Added copyright and license

--- tests/Bronto/Tests/Api/Contact/ContactTest.php
+++ tests/Bronto/Tests/Api/Contact/ContactTest.php
@@ -1,6 +1,9 @@
 <?php
 
 /**
+ * @copyright  2011-2013 Bronto Software, Inc.
+ * @license http://opensource.org/licenses/OSL-3.0 Open Software License v. 3.0 (OSL-3.0)
+ * 
  * @group object
  * @group contact
  */
