1282e9d9ac17c7e2b0d35bd663cab985c12e7641
brontodavejohnson <dave.johnson@bronto.com>
2013-04-09 17:26:20 -0300

    Added copyright and license

--- src/Bronto/Util/Retryer/RetryerException.php
+++ src/Bronto/Util/Retryer/RetryerException.php
@@ -1,5 +1,8 @@
 <?php
-
+/**
+ * @copyright  2011-2013 Bronto Software, Inc.
+ * @license http://opensource.org/licenses/OSL-3.0 Open Software License v. 3.0 (OSL-3.0)
+ */
 class Bronto_Util_RetryerException extends RuntimeException
 {
 
