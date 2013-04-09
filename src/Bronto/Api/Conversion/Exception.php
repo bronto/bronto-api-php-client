43231e246660d7691c5396954604424d603c74d8
brontodavejohnson <dave.johnson@bronto.com>
2013-04-09 16:32:18 -0300

    Added copyright and license

--- src/Bronto/Api/Conversion/Exception.php
+++ src/Bronto/Api/Conversion/Exception.php
@@ -1,5 +1,8 @@
 <?php
-
+/**
+ * @copyright  2011-2013 Bronto Software, Inc.
+ * @license http://opensource.org/licenses/OSL-3.0 Open Software License v. 3.0 (OSL-3.0)
+ */
 class Bronto_Api_Conversion_Exception extends Bronto_Api_Exception
 {
     const DUPLICATE_ORDER  = 901;	// Duplicate Order Id: %%id%%.
