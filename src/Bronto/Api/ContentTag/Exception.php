7f101622a9b80f1bd857ebacf9794b3d08148c03
Juraj Seffer <juraj@boticca.com>
2014-12-03 16:41:33 +0000

    Fixes exception class name

--- src/Bronto/Api/ContentTag/Exception.php
+++ src/Bronto/Api/ContentTag/Exception.php
@@ -4,7 +4,7 @@
  * @copyright  2011-2013 Bronto Software, Inc.
  * @license http://opensource.org/licenses/OSL-3.0 Open Software License v. 3.0 (OSL-3.0)
  */
-class Bronto_Api_Message_Exception extends Bronto_Api_Exception
+class Bronto_Api_ContentTag_Exception extends Bronto_Api_Exception
 {
     const INVALID_CONTENTTAG        = 1601; // The content tag specified is invalid.
     const MISSING_NAME              = 1602; // You must specify a name.
